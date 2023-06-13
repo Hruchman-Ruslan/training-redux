4c33406a5480169cbf3c051715cbc590470880bc
Ruslan <Giovingo92@gmail.com>
2023-06-13 23:30:23 +0300

    Rewrote on Redux Toolkit reducer and store

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,4 +1,57 @@
-import { combineReducers } from 'redux';
+//=============== Before ========================
+
+// import { combineReducers } from 'redux';
+// import { statusFilters } from './constants';
+
+// const taskInitialState = [
+//   { id: 0, text: 'Learn HTML and CSS', completed: true },
+//   { id: 1, text: 'Get good at JavaScript', completed: true },
+//   { id: 2, text: 'Master React', completed: false },
+//   { id: 3, text: 'Discover Redux', completed: false },
+//   { id: 4, text: 'Build amazing apps', completed: false },
+// ];
+
+// const taskReducer = (state = taskInitialState, action) => {
+//   switch (action.type) {
+//     case 'tasks/addTask':
+//       return [...state, action.payload];
+//     case 'tasks/deleteTask':
+//       return state.filter(task => task.id !== action.payload);
+//     case 'tasks/toggleCompleted':
+//       return state.map(task => {
+//         if (task.id !== action.payload) {
+//           return task;
+//         }
+//         return { ...task, completed: !task.completed };
+//       });
+//     default:
+//       return state;
+//   }
+// };
+
+// const filtersInitialState = {
+//   status: statusFilters.all,
+// };
+
+// const filterReducer = (state = filtersInitialState, action) => {
+//   switch (action.type) {
+//     case 'filters/setStatusFilter':
+//       return {
+//         ...state,
+//         status: action.payload,
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
+// export const rootReducer = combineReducers({
+//   tasks: taskReducer,
+//   filters: filterReducer,
+// });
+
+//=============== After ========================
+
 import { statusFilters } from './constants';
 
 const taskInitialState = [
@@ -9,7 +62,7 @@ const taskInitialState = [
   { id: 4, text: 'Build amazing apps', completed: false },
 ];
 
-const taskReducer = (state = taskInitialState, action) => {
+export const taskReducer = (state = taskInitialState, action) => {
   switch (action.type) {
     case 'tasks/addTask':
       return [...state, action.payload];
@@ -31,7 +84,7 @@ const filtersInitialState = {
   status: statusFilters.all,
 };
 
-const filterReducer = (state = filtersInitialState, action) => {
+export const filterReducer = (state = filtersInitialState, action) => {
   switch (action.type) {
     case 'filters/setStatusFilter':
       return {
@@ -42,11 +95,6 @@ const filterReducer = (state = filtersInitialState, action) => {
       return state;
   }
 };
-
-export const rootReducer = combineReducers({
-  tasks: taskReducer,
-  filters: filterReducer,
-});
 
 // \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,19 @@
-import { devToolsEnhancer } from '@redux-devtools/extension';
-import { createStore } from 'redux';
-import { rootReducer } from './reducer';
+//=============== Before ========================
 
-const enhancer = devToolsEnhancer();
-export const store = createStore(rootReducer, enhancer);
+// import { devToolsEnhancer } from '@redux-devtools/extension';
+// import { createStore } from 'redux';
+// import { rootReducer } from './reducer';
+
+// const enhancer = devToolsEnhancer();
+// export const store = createStore(rootReducer, enhancer);
+
+//=============== After ========================
+import { configureStore } from '@reduxjs/toolkit';
+import { filterReducer, taskReducer } from './reducer';
+
+export const store = configureStore({
+  reducer: {
+    tasks: taskReducer,
+    filters: filterReducer,
+  },
+});
