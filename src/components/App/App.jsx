a1565c21dc57736455728b00bfa9b6d9b26e86ec
Ruslan <Giovingo92@gmail.com>
2023-06-13 12:53:55 +0300

    edded global styles

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,9 @@
+import { Section } from 'components/Section/Section.styled';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
+    <Section>
+      <h1>Hello</h1>
+    </Section>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Global } from '@emotion/react';
+
+import 'modern-normalize';
+import { GlobalStyles } from './styles/GlobalStyles.styled';
+
 import { App } from './components/App/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Global styles={GlobalStyles} />
     <App />
   </React.StrictMode>
 );
